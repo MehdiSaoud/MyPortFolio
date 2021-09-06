0b44db2c26d2de5cd0826fbbecaef2c47dfbb653
Mehdi Saoud <saoudmehdi5@gmail.com>
2021-09-06 17:56:51 +0200

    add maison-vapeur project

--- src/js/homescript.js
+++ src/js/homescript.js
@@ -8,9 +8,9 @@ var burger = document.getElementById('burger');
 var nav = document.getElementById('nav');
 var sidebar = document.getElementById('sidebar');
 
-var changeBurgerIds = ['hajime', 'pastry', 'portfolio', 'js_projects', 'skills', 'about'];
+var changeBurgerIds = ['maison-vapeur','roleplay', 'hajime', 'pastry', 'portfolio', 'js_projects', 'skills', 'about'];
 
-var scrollIds = ['roleplay', 'hajime', 'pastry', 'portfolio', 'js_projects', 'skills'];
+var scrollIds = ['maison-vapeur', 'roleplay', 'hajime', 'pastry', 'portfolio', 'js_projects', 'skills'];
 
 
 open.addEventListener('click', function() {
